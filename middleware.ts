ba43fadfba4c3288d3969b6777fce0081925352b
Kevin Martin <martinkevinmichael@gmail.com>
2025-10-24 13:31:27 -0600

    small change

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,9 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
+
+  
+
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET,
